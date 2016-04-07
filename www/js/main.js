588b7442082773b333feafcb9e782b72cd568762
Andre Christoga <andrechristoga@gmail.com>
2016-04-07 17:15:08 +0700

    Comments on javascript

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,13 @@
 function submitForm() {
+  // Get input value
   var first = $('#first').val();
   var last = $('#last').val();
 
   if (first === '' || last === '') {
+    // Error message
     alert('Input is not filled');
   } else {
+    // Success message
     alert('Your name is: ' + first + last)
   }
 }
